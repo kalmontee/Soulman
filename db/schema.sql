d76b8772cdd6c57d6fadcfd7b328f0c6eed63347
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-12-05 11:10:23 -0500

    Setting up the database

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+id INTEGER NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(255) NULL,
+devoured BOOLEAN,
+ PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger) 
+VALUES ('Steak Egg Burger');
+
+INSERT INTO burgers (burger) 
+VALUES ('Cheese Burger');
+
+INSERT INTO burgers (burger) 
+VALUES ('Veggie Burger');
